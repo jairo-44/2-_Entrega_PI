76b87dd5515e8d253d9cfff28be0f1b7e202a329
Bianca Silva <141962456+silvbianca@users.noreply.github.com>
2023-11-18 18:42:47 -0300

    Ajuste na linha 44

--- area_aluno.php
+++ area_aluno.php
@@ -11,7 +11,7 @@ include_once("conexao.php")
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    
+
     <title>Área do aluno</title>
 
     <style>
